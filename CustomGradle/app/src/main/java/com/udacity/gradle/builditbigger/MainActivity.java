c130adaedd9fa02b58ab14b7452d76fdcf7129f2
Paul <paulgureghian@att.net>
2016-07-14 18:54:47 -0700

    7-14-16    7:00 PM

--- CustomGradle/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ CustomGradle/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,12 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
+import com.example.jokesdisplaylibrary.ImageActivity;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -21,24 +22,26 @@ public class MainActivity extends ActionBarActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    public void tellJoke(View view){
+    public void tellJoke(View view) {
         Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+    }
+
+    public void launchLibraryActivity(View view) {
+        Intent intent = new Intent(this, ImageActivity.class);
+        startActivity(intent);
     }
 }
--- CustomGradle/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ CustomGradle/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.example.JokesSupply;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
