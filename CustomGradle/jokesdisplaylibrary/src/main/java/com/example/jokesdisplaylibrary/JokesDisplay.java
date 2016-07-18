2d080cae22c6ef17a5cd275aa383885dda7e6a19
Paul <paulgureghian@att.net>
2016-07-17 17:27:56 -0700

    7-17-16  5:30 PM

--- CustomGradle/jokesdisplaylibrary/src/main/java/com/example/jokesdisplaylibrary/JokesDisplay.java
+++ CustomGradle/jokesdisplaylibrary/src/main/java/com/example/jokesdisplaylibrary/JokesDisplay.java
@@ -2,6 +2,7 @@ package com.example.jokesdisplaylibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.JokesSupply;
 
@@ -11,6 +12,11 @@ public class JokesDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes_display);
+
+        JokesSupply jokesSupply = new JokesSupply();
+
+    //    textView.setText(jokesSupply.getJoke());
+        TextView textView = (TextView) findViewById(R.id.display);
     }
 
     public String getJoke() {
