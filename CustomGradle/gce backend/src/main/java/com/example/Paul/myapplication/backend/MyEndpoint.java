a52b3db48742863d4a54db6fa7fd7b246067861d
Paul <paulgureghian@att.net>
2016-07-18 15:09:18 -0700

    7-18-16  3:00 PM

--- CustomGradle/gce backend/src/main/java/com/example/Paul/myapplication/backend/MyEndpoint.java
+++ CustomGradle/gce backend/src/main/java/com/example/Paul/myapplication/backend/MyEndpoint.java
@@ -1,11 +1,9 @@
-
 package com.example.Paul.myapplication.backend;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import javax.inject.Named;
+import com.example.JokesSupply;
 
 @Api(
         name = "myApi",
@@ -18,10 +16,13 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    JokesSupply jokesSupply = new JokesSupply();
+
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        String joke = jokesSupply.getJoke();
+        response.setData(joke);
 
         return response;
     }
