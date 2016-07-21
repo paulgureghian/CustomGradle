cba46d394878272be756af4d2fa6bff5c3f03ea8
Paul <paulgureghian@att.net>
2016-07-21 14:19:24 -0700

    7-21-16    2:20  PM

--- CustomGradle/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ CustomGradle/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -3,9 +3,29 @@ package com.udacity.gradle.builditbigger;
 import static org.junit.Assert.*;
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 
 public class EndpointsAsyncTaskTest {
+
+    EndpointsAsyncTask asyncTask = new EndpointsAsyncTask();
+
+    @Test
+    public void AsyncTaskTest() throws Exception {
+
+        String result = asyncTask.execute().get(60, TimeUnit.SECONDS);
+
+        assertNotNull(result);
+
+
+    }
+
+
 
     @org.junit.Test
     public void testDoInBackground() throws Exception {
