37cd6d8c23aaf5b9adb622e54740935fafb08b94
Paul <paulgureghian@att.net>
2016-07-24 19:00:10 -0700

    7-24-16    7:00 PM

--- CustomGradle/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ CustomGradle/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,11 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.example.JokesSupply;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 public class MainActivityFragment extends Fragment {
 
@@ -21,12 +16,6 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
         return root;
     }
 }
