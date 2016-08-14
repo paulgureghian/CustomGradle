e8b6295e0ea81a23c844fd458995275fbe84983a
Paul <paulgureghian@att.net>
2016-08-14 15:43:29 -0700

    8-14-16   3:20 PM

---
+++ gce backend/src/main/java/com/example/Paul/myapplication/backend/MyBean.java
@@ -0,0 +1,14 @@
+package com.example.Paul.myapplication.backend;
+
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
